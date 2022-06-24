15112be8f5b55eb19b9c67dd46bc1019303ee6b4
Koichi <koichi89592@gmail.com>
2022-06-23 20:37:46 -0700

    Add image assets and metadata

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      {/* <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
       <h1>FirstPost</h1>
       <h2>
         <Link href="/">
