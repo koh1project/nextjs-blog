26f056c034df96516cfaa9a9633911fd3562d66a
Koichi <koichi89592@gmail.com>
2022-06-23 22:12:47 -0700

    Added SSG contents

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,11 +20,28 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, this is my Next.js practice site</p>
-        <p>I have just heard that Next.js is useful, but I now understand what Next.js convenient is</p>
+        <p>
+          I have just heard that Next.js is useful, but I now understand what
+          Next.js convenient is
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
